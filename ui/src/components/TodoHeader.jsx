6f750677e266666ea447aa9b162e900df3d27d7a
nam hoang <nam.hoang@ntq-solution.com.vn>
2020-12-21 18:28:24 +0700

    [~] dd

--- ui/src/components/TodoHeader.jsx
+++ ui/src/components/TodoHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row, Col, Button, Input, Typography } from 'antd';
+import { Row, Col, Button, Input, Typography, notification  } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { nanoid } from 'nanoid';
 import useSWR, { mutate } from 'swr';
@@ -9,13 +9,20 @@ const { Title } = Typography;
 const fetcher = (url) => axios.get(url).then(res => res.data);
 const baseURL = 'http://localhost:4000/todos';
 
+const openNotificationWithIcon = (type, mssg, desc) => {
+  notification[type]({
+    message: mssg,
+    duration: 3,
+    description: desc,
+  });
+};
+
 export const TodoHeader = () => {
   const [task, setTask] = React.useState('');
   const { data } = useSWR(baseURL, fetcher);
 
   //NOTE: https://swr.vercel.app/docs/mutation#mutation-and-post-request
   const onAdd  = async () => {
-    mutate(baseURL, [...data, {title: task, id: nanoid(), complete: false }], false);
     await axios({
       url: baseURL,
       method: 'POST',
@@ -25,7 +32,9 @@ export const TodoHeader = () => {
         complete: false
       }
     });
+    mutate(baseURL, [...data, {title: task, id: nanoid(), complete: false }], false);
     setTask('');
+    openNotificationWithIcon('success', 'Successfully', 'Added an item to todo-list 👍');
   }
 
   return (
